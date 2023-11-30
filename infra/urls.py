f9189a80cda73c127c1cbd0828f9ec6d06a0b5ff
Ryo23sanyu <dobokuka4@gmail.com>
2023-11-30 21:32:06 +0900

    Infraproject

--- infra/urls.py
+++ infra/urls.py
@@ -3,10 +3,10 @@ from .import views
 
 urlpatterns = [
     path('', views.index_view, name='index'),
-    path('infra/<int:pk>/work_list/', views.WorkListInfraView.as_view(), name='work_list-infra'),
     path('infra/', views.ListInfraView.as_view(), name='list-infra'),
     path('infra/<int:pk>/detail/', views.DetailInfraView.as_view(), name='detail-infra'),
     path('infra/create/', views.CreateInfraView.as_view(), name='create-infra'),
     path('infra/<int:pk>/delete/', views.DeleteInfraView.as_view(), name='delete-infra'),
     path('infra/<int:pk>/update/', views.UpdateInfraView.as_view(), name='update-infra'),
+    path('infra/<int:pk>/article/', views.ArticleInfraView.as_view(), name='article-infra'),
 ]
--- infra/views.py
+++ infra/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import WorkListView, ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView, ArticleView
 from .models import Infra
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-class WorkListInfraView(LoginRequiredMixin, WorkListView):
-  template_name = 'infra/infra_work_list.html'
-  model = Infra
-  
+ 
 class ListInfraView(LoginRequiredMixin, ListView):
   template_name = 'infra/infra_list.html'
   model = Infra
-  
+
 class DetailInfraView(LoginRequiredMixin, DetailView):
   template_name = 'infra/infra_detail.html'
   model = Infra
@@ -37,3 +33,7 @@ def index_view(request):
   order_by = request.GET.get('order_by', 'span_number')
   object_list = Infra.objects.order_by(order_by)
   return render(request, 'infra/index.html', {'object_list': object_list})
+
+class ArticleInfraView(ArticleView):
+  template_name = 'infra/infra_article.html'
+  model = Infra
