9ac6849a394116557165827204042e04eaec718a
Ryo23sanyu <dobokuka4@gmail.com>
2023-12-02 22:04:20 +0900

    Infraproject

--- accounts/views.py
+++ accounts/views.py
@@ -8,4 +8,4 @@ class SignupView(CreateView):
   model = User
   form_class = SignupForm
   template_name ='accounts/signup.html'
-  success_url = reverse_lazy('list-article')
+  success_url = reverse_lazy('index')
